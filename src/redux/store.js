62f004ee695e803b1d29014fef8871909bab9134
Em Streżyńska <emstrezynska@gmail.com>
2019-11-28 18:00:14 +0100

    small fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ export const store = createStore(
     reducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 
     )
 );
